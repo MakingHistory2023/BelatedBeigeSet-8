36baf40882b7281f7c404412c857b24677a53355
MakingHistory2023 <143676019+MakingHistory2023@users.noreply.github.com>
2023-09-17 20:02:02 +0000

    Initial commit

---
+++ main.cs
@@ -0,0 +1,15 @@
+using System;
+using static System.Console;
+namespace InchesToCentimeters {
+  class Mainclass
+  {
+  public static void Main (string[] args) {
+    {
+      int inches=3;
+      const double centimeters=2.54;
+      double result=inches*centimeters;
+      
+    }
+    Console.WriteLine("{0} inches is {1} centimeters,"inches,result);}
+  }
+}
